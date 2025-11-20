8aa3256fe1738f3b340d3d038d9dc5cd8c5e4612
Richter <jakob.richter@bs.nttdata.com>
2025-11-20 10:52:52 +0100

    added functionality to connect to the local API of the other project, currently the output ist determined solely by the other project

---
+++ src/main/java/de/jbg/ui/Application.java
@@ -0,0 +1,4 @@
+package de.jbg.ui;
+
+public class Application {
+}
--- src/main/java/de/jbg/ui/Main.java
+++ src/main/java/de/jbg/ui/Main.java
@@ -1,28 +1,10 @@
 package de.jbg.ui;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("JBG 3.5");
-        JPanel panel = new JPanel();
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 500);
-        frame.setVisible(true);
-        BorderLayout layout = new BorderLayout();
-        panel.setLayout(layout);
-        frame.add(panel, "Center");
-
-        JButton buttonLoad = new JButton("Eintrag laden");
-        panel.add(buttonLoad, "South");
-        buttonLoad.setPreferredSize(new Dimension(150, 30));
-        buttonLoad.setVisible(true);
-
-//        JButton test = new JButton("test");
-//        panel.add(test, "South");
-//        test.setSize(150, 30);
-//        test.setVisible(true);
+    public static void main(String[] args) throws Exception {
+        memeClient client = new memeClient();
+        System.out.println(client.getAll());
     }
+
 }
