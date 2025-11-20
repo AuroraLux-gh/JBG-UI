4c4e6a540f0ee5c42be9f0f92b3a2f6376574c7a
Lux <auroralux.pruss@bs.nttdata.com>
2025-11-20 13:57:27 +0100

    UI an image angepasst

--- src/main/java/de/jbg/ui/Main.java
+++ src/main/java/de/jbg/ui/Main.java
@@ -1,8 +1,10 @@
 package de.jbg.ui;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         UI.lossHihi();
         memeClient client = new memeClient();
         System.out.println(client.getAll());
--- src/main/java/de/jbg/ui/UI.java
+++ src/main/java/de/jbg/ui/UI.java
@@ -1,7 +1,11 @@
 package de.jbg.ui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
+import java.io.File;
+import java.io.IOException;
 
 public class UI {
     public static void lossHihi() {
@@ -10,7 +14,7 @@ public class UI {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 500);
-        frame.setVisible(true);
+
         BorderLayout layout = new BorderLayout();
         panel.setLayout(layout);
         frame.add(panel, "Center");
@@ -18,6 +22,18 @@ public class UI {
         JButton buttonLoad = new JButton("Eintrag laden");
         panel.add(buttonLoad, "South");
         buttonLoad.setPreferredSize(new Dimension(150, 30));
+
+        BufferedImage bImage = null;
+        try {
+            bImage = ImageIO.read(new File("testbild.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Image image1 = bImage.getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        ImageIcon image = new ImageIcon(image1);
+        panel.add(new JLabel(image));
+
         buttonLoad.setVisible(true);
+        frame.setVisible(true);
     }
 }
