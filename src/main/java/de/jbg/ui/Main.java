840a667833362248afbab31d9cc434b747377966
Lux <auroralux.pruss@bs.nttdata.com>
2025-11-21 08:42:29 +0100

    Änderung methodennamen

--- src/main/java/de/jbg/ui/Main.java
+++ src/main/java/de/jbg/ui/Main.java
@@ -1,29 +1,26 @@
 package de.jbg.ui;
 
 
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) throws Exception, InterruptedException {
 
-        Scanner scanner = new Scanner(System.in);
-        UI.lossHihi();
+//        Scanner scanner = new Scanner(System.in);
+        UI.showUI();
 
-        memeClient client = new memeClient();
+//        memeClient client = new memeClient();
 
-        System.out.println("Please choose which data you want to GET, (0) all data in Meme, (1) a specific Meme (you will have to insert an ID): ");
-        switch(scanner.nextInt()) {
-            case 0:
-                System.out.println(client.getAll());
-                break;
-            case 1:
-                System.out.println("Please mention the ID (e.g. '1') of the meme you want to get: ");
-                Integer memeID = scanner.nextInt();
-                System.out.println(client.findById(memeID));
-                break;
-        }
+//        System.out.println("Please choose which data you want to GET, (0) all data in Meme, (1) a specific Meme (you will have to insert an ID): ");
+//        switch(scanner.nextInt()) {
+//            case 0:
+//                System.out.println(client.getAll());
+//                break;
+//            case 1:
+//                System.out.println("Please mention the ID (e.g. '1') of the meme you want to get: ");
+//                Integer memeID = scanner.nextInt();
+//                System.out.println(client.findById(memeID));
+//                break;
+//        }
 
     }
 
